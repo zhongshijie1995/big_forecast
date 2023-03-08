66aa8497757e14d0c5c690b72a4d6680fd71c7fa
钟世杰 <zhongshijie1995@outlook.com>
2023-03-08 22:08:06 +0800

    调整阈值搜索

--- _ML/metrics.py
+++ _ML/metrics.py
@@ -78,15 +78,15 @@ class Metrics:
             :return: F1最大化的值
             """
             pred_prob, act_true, threshold = args
-            return 1 - Metrics.f1_metrics(pred_prob, act_true, threshold)[1]
+            return - Metrics.f1_metrics(pred_prob, act_true, threshold)[1]
 
         space = hp.choice(
-            'min(1-f1)',
+            'min(-f1)',
             [
                 (_y_pred_prob, _y_true, hp.uniform('threshold', 0, 1)),
             ]
         )
-        best = fmin(convert_f1_metrics, space, algo=tpe.suggest, verbose=False, max_evals=100)
+        best = fmin(convert_f1_metrics, space, algo=tpe.suggest, verbose=False, show_progressbar=False, max_evals=100)
         logger.info('最佳验证集为{}，最佳阈值为{}', best['min(1-f1)'], best['threshold'])
         return best['threshold']
 
