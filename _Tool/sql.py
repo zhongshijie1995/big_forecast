6728889fc7a3a7b08d6c1788f09b118263770d37
钟世杰 <zhongshijie1995@outlook.com>
2023-03-18 14:02:03 +0800

    调整pandas与sqlite交互-修复bug

--- _Tool/sql.py
+++ _Tool/sql.py
@@ -5,10 +5,10 @@ import sqlalchemy
 class DataframeSQL:
     @staticmethod
     def df_into_sqlite(db_path_name: str, table_name: str, df: pd.DataFrame) -> None:
-        engine = sqlalchemy.create_engine('sqlite:///'.format(db_path_name))
+        engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path_name))
         df.to_sql(table_name, engine)
 
     @staticmethod
     def sqlite_into_df(db_path_name: str, table_name: str) -> pd.DataFrame:
-        engine = sqlalchemy.create_engine('sqlite:///'.format(db_path_name))
+        engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path_name))
         return pd.read_sql(table_name, engine)
