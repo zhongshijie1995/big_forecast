48c374ef186e0acc57ba4ea7724b93d12f7ac8c2
钟世杰 <zhongshijie1995@outlook.com>
2023-03-19 13:15:10 +0800

    调整pandas与sqlite交互-增加查询借口

--- _Tool/sql.py
+++ _Tool/sql.py
@@ -4,11 +4,17 @@ import sqlalchemy
 
 class DataframeSQL:
     @staticmethod
-    def df_into_sqlite(db_path_name: str, table_name: str, df: pd.DataFrame) -> None:
-        engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path_name))
-        df.to_sql(table_name, engine, index=False)
+    def __get_sqlite_engine(db_path_name) -> sqlalchemy.engine.Engine:
+        return sqlalchemy.create_engine('sqlite:///{}'.format(db_path_name))
 
     @staticmethod
-    def sqlite_into_df(db_path_name: str, table_name: str) -> pd.DataFrame:
-        engine = sqlalchemy.create_engine('sqlite:///{}'.format(db_path_name))
-        return pd.read_sql(table_name, engine)
+    def df_into_sqlite(db_path_name: str, table_name: str, df: pd.DataFrame) -> None:
+        engine = DataframeSQL.__get_sqlite_engine(db_path_name)
+        with engine.connect() as conn:
+            df.to_sql(table_name, conn, index=False)
+
+    @staticmethod
+    def sqlite_query_into_df(db_path_name: str, query: str) -> pd.DataFrame:
+        engine = DataframeSQL.__get_sqlite_engine(db_path_name)
+        with engine.connect() as conn:
+            return pd.read_sql_query(query, conn)
