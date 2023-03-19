f1dc856606cdf5cb39463451130bec1165919d02
钟世杰 <zhongshijie1995@outlook.com>
2023-03-19 14:57:52 +0800

    增加日志格式设置器

--- _Tool/log.py
+++ _Tool/log.py
@@ -1,71 +1,17 @@
-import time
-from typing import Callable, Any, Dict, Tuple
-
+import sys
 from loguru import logger
 
 
-class FuncLog:
-    """
-    函数日志打印器
-    """
-
+class LoggerFormat:
     @staticmethod
-    def cost(func: Callable) -> Any:
-        """
-        开销记录日志
-
-        :param func: 待执行函数
-
-        :return:
-        """
-
-        def flat_args(args: Tuple[Any], kwargs: Dict[str, Any]) -> str:
-            """
-            压平参数
-
-            :param args: 普通参数
-            :param kwargs: 关键字参数
-
-            :return: 返回参数后的字符
-            """
-            tmp = []
-            for arg in args:
-                tmp.append(str(arg).replace('\n', ''))
-            for kw, arg in kwargs.items():
-                tmp.append('{}={}'.format(kw, str(arg).replace('\n', '')))
-            return ', '.join(tmp)
-
-        def wrapper(*args, **kwargs) -> Any:
-            """
-            切面函数
-
-            :param args: 传入参数
-            :param kwargs: 传入参数
-
-            :return: 函数执行结果
-            """
-            # 获取函数名称
-            func_name = str(func.__name__)
-            # 获取函数文档摘要
-            func_docs = str(func.__doc__)
-            summary = '' if func_docs is None else func_docs.strip().split()[0]
-            # 打印开始标记
-            logger.debug('执行[{}]({})，传入参数[{}]', func_name, summary, flat_args(args, kwargs))
-            # 登记开始执行时间
-            start_time = time.time()
-            # 执行函数
-            result = func(*args, **kwargs)
-            # 登记结束时间
-            end_time = time.time()
-            # 计算耗时
-            cost_time = round(end_time - start_time, 1)
-            # 打印结束标记
-            logger.debug('结束[{}]，共耗时[{}]秒', func_name, cost_time)
-            return result
-
-        # 返回切面函数
-        return wrapper
-
-
-if __name__ == '__main__':
-    pass
+    def set(format_str_list: str = None) -> None:
+        if format_str_list is None:
+            format_str_list = [
+                '<green>{time:YYYY-MM-DD HH:mm:ss}</green>',
+                '<cyan>{module}.{function}:{line}</cyan>',
+                '<yellow>{message}</yellow>'
+            ]
+        logger.remove()
+        logger.add(sys.stderr, format='| '.join(format_str_list))
+        logger.info('日志格式指定成功')
+        return None
