b5b8b4e6e0b618c44eb9f72ac887abb2c8d0c63f
zhongshijie <zhongshijie1995@outlook.com>
2023-10-22 21:47:13 +0800

    增加文件合并功能

--- _Tool/FileIO.py
+++ _Tool/FileIO.py
@@ -4,7 +4,7 @@ import os
 
 class B64IO:
     """
-    Base64输入输出工具
+    Base64输入输出
     """
 
     @staticmethod
@@ -38,7 +38,7 @@ class B64IO:
         return result
 
     @staticmethod
-    def b64file_split(b64_path: str, b64_part_path: str, part_size: int = 204800) -> str:
+    def b64_split(b64_path: str, b64_part_path: str, part_size: int = 204800) -> str:
         """
         Base64文件分割
 
@@ -57,7 +57,7 @@ class B64IO:
         return b64_part_path
 
     @staticmethod
-    def b64file_merge(b64_part_path: str, b64_path: str):
+    def b64_merge(b64_part_path: str, b64_path: str):
         """
         Base64文件合并
 
@@ -72,5 +72,34 @@ class B64IO:
             with open(i, 'r') as f:
                 result += f.read()
         with open(b64_path, 'w+') as f:
+            f.write(result)
+        return result
+
+
+class TxtIO:
+    """
+    txt输入输出
+    """
+
+    @staticmethod
+    def txt_merge(txt_part_path: str, txt_path: str, new_line: bool = True):
+        """
+        txt文件合并
+
+        :param txt_part_path: txt分割文件路径
+        :param txt_path: txt文件路径
+        :param new_line: 文件换行
+        :return: 合并结果
+        """
+        file_path_list = [os.path.join(txt_part_path, i) for i in os.listdir(txt_part_path)]
+        file_path_list.sort()
+        result = ''
+        for i in file_path_list:
+            with open(i, 'r') as f:
+                new_content = f.read()
+                if new_line and not new_content.endswith('\n'):
+                    new_content += '\n'
+                result += new_content
+        with open(txt_path, 'w+') as f:
             f.write(result)
         return result
